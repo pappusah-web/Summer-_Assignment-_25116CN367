710b3fe8d0fb28889ce3cc853db4d61229ef1f91
Pappu <25116cn367@gmail.com>
2026-06-07 11:35:52 +0530

    Day-7 uplodaded.

---
+++ Day-7/04_program.cpp
@@ -0,0 +1,18 @@
+//Write a program to Recursive reverse number. 
+#include <iostream>
+using namespace std;
+
+int reverseNumber(int n, int rev = 0)
+{
+    if (n == 0)
+        return rev;
+    return reverseNumber(n / 10, rev * 10 + n % 10);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << reverseNumber(n);
+    return 0;
+}
