e8ac5e931f524cccd9f8e790f64849d7db079128
Pappu <25116cn367@gmail.com>
2026-06-21 23:33:24 +0530

    Day-21 uplodaded.

---
+++ Day-21/03_program.cpp
@@ -0,0 +1,26 @@
+// Write a program to Count vowels and consonants. 
+#include <iostream>
+using namespace std;
+
+int main() {
+    char str[100];
+    int vowels = 0, consonants = 0;
+
+    cout << "Enter a string: ";
+    cin.getline(str, 100);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' ||
+            str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U') {
+            vowels++;
+        }
+        else if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')) {
+            consonants++;
+        }
+    }
+
+    cout << "Vowels = " << vowels << endl;
+    cout << "Consonants = " << consonants << endl;
+
+    return 0;
+}
